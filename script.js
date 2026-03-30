4462c28d4f5bea6ddbaf64b5cacf1172bf3c892c
Georgi Vasilev <georgegv04@gmail.com>
2026-03-30 13:25:44 +0300

    initialize project

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+
+    case "-":
+      return subtract(num1, num2);
+
+    case "*":
+      return multiply(num1, num2);
+
+    case "/":
+      return divide(num1, num2);
+  }
+}
+
+// console.log(operate(4, 2, "/"));
